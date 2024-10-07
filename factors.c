2503493c5a54e1361022c6bf0d6a62e879e94fca
Shighi <daisyshighi@gmail.com>
2024-10-06 23:59:53 -0700

    large number

--- factors.c
+++ factors.c
@@ -2,47 +2,78 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <limits.h>
+#include <gmp.h>
+
+/**
+ * handle_large_number - Process numbers that exceed unsigned long long
+ * @num: String containing the large number
+ */
+void handle_large_number(char *num)
+{
+	mpz_t n, i, remainder;
+	mpz_inits(n, i, remainder, NULL);
+	mpz_set_str(n, num, 10);
+
+	/* Check small factors first for performance */
+	int small_factors[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+	int len = sizeof(small_factors) / sizeof(small_factors[0]);
+
+	for (int j = 0; j < len; j++)
+	{
+		mpz_mod_ui(remainder, n, small_factors[j]);
+		if (mpz_cmp_ui(remainder, 0) == 0)
+		{
+			mpz_divexact_ui(i, n, small_factors[j]);
+			gmp_printf("%Zd=%Zd*%d\n", n, i, small_factors[j]);
+			mpz_clears(n, i, remainder, NULL);
+			return;
+		}
+	}
+
+	mpz_clears(n, i, remainder, NULL);
+	printf("%s=%s*1\n", num, num);
+}
 
 /**
  * trial_division - Find factors using optimized trial division
  * @num: String containing the number to factorize
- *
- * Description: Uses wheel factorization to skip numbers that can't be factors
  */
 void trial_division(char *num)
 {
 	unsigned long long n;
 	unsigned long long i;
-	const unsigned long long small_primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-	size_t len = sizeof(small_primes) / sizeof(small_primes[0]);
+	char *endptr;
 	
-	n = strtoull(num, NULL, 10);
-	
-	/* Check small primes first */
-	for (size_t j = 0; j < len; j++)
+	/* Check if number is too large or invalid */
+	if (strlen(num) > 19)
 	{
-		if (n % small_primes[j] == 0)
-		{
-			printf("%llu=%llu*%llu\n", n, n / small_primes[j], small_primes[j]);
-			return;
-		}
+		handle_large_number(num);
+		return;
 	}
 	
-	/* Use wheel factorization incrementing pattern */
-	const unsigned long long increments[] = {4, 2, 4, 2, 4, 6, 2, 6};
-	size_t inc_len = sizeof(increments) / sizeof(increments[0]);
-	size_t inc_idx = 0;
+	n = strtoull(num, &endptr, 10);
+	if (*endptr != '\0' || n == ULLONG_MAX)
+	{
+		handle_large_number(num);
+		return;
+	}
 	
-	i = 31;  /* Start after checking small primes */
-	while (i * i <= n)
+	/* Check even numbers first */
+	if (n % 2 == 0)
+	{
+		printf("%llu=%llu*2\n", n, n / 2);
+		return;
+	}
+	
+	/* Check small odd numbers */
+	for (i = 3; i * i <= n; i += 2)
 	{
 		if (n % i == 0)
 		{
 			printf("%llu=%llu*%llu\n", n, n / i, i);
 			return;
 		}
-		i += increments[inc_idx];
-		inc_idx = (inc_idx + 1) % inc_len;
 	}
 	
 	printf("%llu=%llu*1\n", n, n);
@@ -52,7 +83,6 @@ void trial_division(char *num)
  * main - Entry point of the factors program
  * @argc: Argument count
  * @argv: Array of argument strings
- *
  * Return: 0 on success, 1 on failure
  */
 int main(int argc, char *argv[])
@@ -60,7 +90,7 @@ int main(int argc, char *argv[])
 	FILE *file;
 	char line[1024];
 	clock_t start = clock();
-	double time_limit = 4.5;  /* Set slightly below 5s to ensure completion */
+	double time_limit = 4.5;
 	
 	if (argc != 2)
 		return (1);
